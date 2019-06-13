d2cab93988f5ab10cae5a4a61f98980bd3efaeff
nipantelic <nikola.pantelic@eng.it>
2019-06-13 12:19:38 +0200

    Add project files.

---
+++ TechSupport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TechSupport.Startup))]
+namespace TechSupport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
