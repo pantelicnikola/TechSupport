ef1ff2a75124516813962f04a12ce47a1fd4436f
pantelicnikola <pantelic.n94@gmail.com>
2019-06-16 17:39:40 +0200

    assign role when register

--- TechSupport/Controllers/AccountController.cs
+++ TechSupport/Controllers/AccountController.cs
@@ -161,6 +161,7 @@ namespace TechSupport.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    await UserManager.AddToRoleAsync(user.Id, "user");
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
