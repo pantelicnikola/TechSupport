5cf71aa311ea2f971e9cca64cca5f9600ec06ba8
nipantelic <nikola.pantelic@eng.it>
2019-06-25 15:54:36 +0200

    add agent signup & signoff to channel

--- TechSupport/Controllers/ChannelsController.cs
+++ TechSupport/Controllers/ChannelsController.cs
@@ -16,28 +16,59 @@ namespace TechSupport.Controllers
         private TechSupport20190613121821_dbEntities db = new TechSupport20190613121821_dbEntities();
 
         // GET: Channels
-        public ActionResult Index()
+        [Authorize(Roles = "user")]
+        public ActionResult MyChannels()
         {
-            var channels = db.Channels.Include(c => c.AspNetUser);
+            var userId = User.Identity.GetUserId();
+            var channels = db.Channels.Include(c => c.AspNetUser).Where(c => c.Creator == userId).Where(c => c.Closed == false);
             return View(channels.ToList());
         }
 
-        // GET: Channels/Details/5
-        public ActionResult Details(int? id)
+        [Authorize(Roles = "agent")]
+        public ActionResult AllChannels()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Channel channel = db.Channels.Find(id);
-            if (channel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(channel);
+            var userId = User.Identity.GetUserId();
+            var channels = db.Channels.Include(c => c.AspNetUser).Where(c => c.Closed == false);
+            return View(channels.ToList());
         }
 
+        [Authorize(Roles = "agent")]
+        public ActionResult SignUp(int id)
+        {
+            Channel channel = db.Channels.Find(id);
+            channel.AspNetUsers.Add(db.AspNetUsers.Find(User.Identity.GetUserId()));
+            db.Entry(channel).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("AllChannels");
+        }
+
+        [Authorize(Roles = "agent")]
+        public ActionResult SignOff(int id)
+        {
+            Channel channel = db.Channels.Find(id);
+            channel.AspNetUsers.Remove(db.AspNetUsers.Find(User.Identity.GetUserId()));
+            db.Entry(channel).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("AllChannels");
+        }
+
+        // GET: Channels/Details/5
+        //public ActionResult Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Channel channel = db.Channels.Find(id);
+        //    if (channel == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(channel);
+        //}
+
         // GET: Channels/Create
+        [Authorize(Roles = "user")]
         public ActionResult Create()
         {
             ViewBag.Creator = new SelectList(db.AspNetUsers, "Id", "Email");
@@ -49,81 +80,99 @@ namespace TechSupport.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "user")]
         public ActionResult Create([Bind(Include = "Id,Name,TimeCreated,Creator,Closed,Price")] Channel channel)
         {
             if (ModelState.IsValid)
             {
+                AspNetUser user = db.AspNetUsers.Find(User.Identity.GetUserId());
+                int channelPrice = db.ChannelPrices.First().NumTokens;
+
                 channel.Closed = false;
-                channel.Creator = User.Identity.GetUserId();
+                channel.Creator = user.Id;
                 channel.TimeCreated = DateTime.Now;
                 channel.Price = 0;
                 db.Channels.Add(channel);
+
+                user.Tokens -= channelPrice;
+                db.Entry(user).State = EntityState.Modified;
+                
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                return RedirectToAction("MyChannels");
             }
 
             ViewBag.Creator = new SelectList(db.AspNetUsers, "Id", "Email", channel.Creator);
             return View(channel);
+        }
+
+        [Authorize(Roles = "user")]
+        public ActionResult Close(int id)
+        {
+            Channel channel = db.Channels.Find(id);
+            channel.Closed = true;
+            db.Entry(channel).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("MyChannels");
         }
 
         // GET: Channels/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Channel channel = db.Channels.Find(id);
-            if (channel == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.Creator = new SelectList(db.AspNetUsers, "Id", "Email", channel.Creator);
-            return View(channel);
-        }
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Channel channel = db.Channels.Find(id);
+        //    if (channel == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    ViewBag.Creator = new SelectList(db.AspNetUsers, "Id", "Email", channel.Creator);
+        //    return View(channel);
+        //}
 
-        // POST: Channels/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Name,TimeCreated,Creator,Closed,Price")] Channel channel)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(channel).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.Creator = new SelectList(db.AspNetUsers, "Id", "Email", channel.Creator);
-            return View(channel);
-        }
+        //// POST: Channels/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "Id,Name,TimeCreated,Creator,Closed,Price")] Channel channel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(channel).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    ViewBag.Creator = new SelectList(db.AspNetUsers, "Id", "Email", channel.Creator);
+        //    return View(channel);
+        //}
 
-        // GET: Channels/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Channel channel = db.Channels.Find(id);
-            if (channel == null)
-            {
-                return HttpNotFound();
-            }
-            return View(channel);
-        }
+        //// GET: Channels/Delete/5
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Channel channel = db.Channels.Find(id);
+        //    if (channel == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(channel);
+        //}
 
-        // POST: Channels/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Channel channel = db.Channels.Find(id);
-            db.Channels.Remove(channel);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //// POST: Channels/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Channel channel = db.Channels.Find(id);
+        //    db.Channels.Remove(channel);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
